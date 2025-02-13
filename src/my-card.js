0fa9cae8c36dd67f287a59e6236990c8fc861a53
macygmac <mgm5927@psu.edu>
2025-02-13 16:00:42 -0500

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -18,13 +18,20 @@ export class MyCard extends LitElement {
     this.img = "https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/a3/96/25/a3962539-6df4-53f8-47f8-e9998cbecd81/859718224273_cover.jpg/1200x1200bf-60.jpg";
     this.description = "Do you like this artist? If you haven't listened to him, give it a try! He is a popular artist, but this album of his is less known.";
     this.link = "https://hax.psu.edu";
+    this.fancy = false;
   
   }
 
   static get styles() {
     return css`
       :host {
+        display: inline-block;
+      }
+      :host([fancy]) .card {
         display: block;
+        background-color: white;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
       }
 
       h1{ 
@@ -42,11 +49,14 @@ p{
   background-color: pink;
   display: inline-block;
   max-width: 400px;
+  height: 600px;
   padding: 20px;
   margin: 10px;
   border: dotted red;
   border-width: 5px;
   border-radius: 10px; 
+  position: relative;
+  overflow: hidden;
 }
 
 a {
@@ -70,10 +80,6 @@ img{
  
 }
 
-.fancy{
- background-color: black;
- color: #FF0099;
-}
 
 
 @media (min-width: 800px), (max-width: 500px) {
@@ -107,21 +113,64 @@ img{
 }
 
 
+details summary {
+    text-align: left;
+    font-size: 20px;
+    padding: 8px 0;
+  }
+
+  details[open] summary {
+    font-weight: bold;
+  }
+  
+  details div {
+    border: 2px solid black;
+    text-align: left;
+    padding: 8px;
+    height: 70px;
+    overflow-y: auto;
+    max-height: 100px;
+    position: auto;
+    bottom: 10px;
+  }
+
+
+
     `;
   }
+
+
+  // put this anywhere on the MyCard class; just above render() is probably good
+  openChanged(e) {
+    console.log(e);
+    if (e.target.getAttribute('open') !== null) {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
+  }
+
 
   render() {
     return html`<div class= "card">
       <h1>${this.title}</h1>
       <img src="${this.img}" alt="${this.title}" />
-      <p>${this.description}</p>
-      <button class="details-button"><a href="${this.link}" target="_blank">Details</a></button>
+      <!-- put this in your render method where you had details -->
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>Description</summary>
+        <div>
+          <slot>${this.description}</slot>
+          <button class="details-button"><a href="${this.link}" target="_blank">Details</a></button>
+        </div>
+        </details>
 
       </div>`;
   }
 
   static get properties() {
     return {
+      fancy: { type: Boolean, reflect: true },
       title: { type: String },
       img: { type: String },
       description: { type: String },
